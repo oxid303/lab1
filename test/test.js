bdd03f969daf6ab9843b3d85cc404b89efa3ae77
Ann <oxid303@gmail.com>
2018-06-21 23:19:17 +0300

    empty test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ var sum = require("../src/Lab1.js");
 var assert = require("assert");
 describe("count", function() {
 	it("count spaces in the string", function() {
-		assert.equal(sum.spaces("serg esr er ht"), 3);
+		assert.equal(sum.spaces(""), 0);
 	});
 	it("count words in the string", function() {
-		assert.equal(sum.words("srt, ht"), 2);
+		assert.equal(sum.words(""), 0);
 	});
 	it("count symbols ( ,.?!-_ ) in the string", function() {
-		assert.equal(sum.symbols("gesr.!!hrt/?srt "), 4);
+		assert.equal(sum.symbols(""), 0);
 	});
 });
