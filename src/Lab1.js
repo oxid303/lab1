f31ddee31f98e5fbdfac5c0dd058ec37ba5d72c1
Ann <oxid303@gmail.com>
2018-07-17 00:52:50 +0300

    tab fix 1

--- src/Lab1.js
+++ src/Lab1.js
@@ -5,25 +5,25 @@ var checkEmpty = require('../utils/check-empty.js');
 
 class Counts {
 
-	constructor(str) {
-		this.str = str;
-	}
+  constructor(str) {
+    this.str = str;
+  }
 
-	get check() {
-		return checkNoString(this.str) || checkEmpty(this.str);
-	}
+  get check() {
+    return checkNoString(this.str) || checkEmpty(this.str);
+  }
 
-	countSpaces() {
-		return new Counts(this.str).check ? 0 : this.str.split(/[ ]/g).length - 1;
-	}
+  countSpaces() {
+    return new Counts(this.str).check ? 0 : this.str.split(/[ ]/g).length - 1;
+  }
 
-	countWords() {
-		return new Counts(this.str).check ? 0 : this.str.split(/\s+\b/).length;
-	}
+  countWords() {
+    return new Counts(this.str).check ? 0 : this.str.split(/\s+\b/).length;
+  }
 
-	countSymbols() {
-		return new Counts(this.str).check ? 0 : this.str.split(/[^ \w]|[_]/g).length -1;
-	}
+  countSymbols() {
+    return new Counts(this.str).check ? 0 : this.str.split(/[^ \w]|[_]/g).length -1;
+  }
 }
 
 module.exports = Counts;
